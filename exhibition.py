df19f3ec07764a6adb82616d9be1fd199db28dd4
anthonyjamous <anthony_jamous@hotmail.com>
2020-04-27 14:59:49 +0200

    script file

--- exhibition.py
+++ exhibition.py
@@ -2,29 +2,41 @@ PATH = 'C:/Users/antho/PycharmProjects/'
 
 
 def readfile(names):
-    for f in names:
-        f = open(PATH + f, 'r')
-        content1 = f.read()
+    # hashmap to store all paintings by characteristic
+    characteristics = {}
+
+    for name in names:
+        f = open(PATH + name, 'r')
         id = 0
         for line in f:
             id += 1
             if id == 1:
                 numberofpaintings = int(line)
             else:
-                paintingstr = line.split(' ')
+                paintingstr = line.rstrip().split(' ')
                 type = paintingstr[0]
                 nbofcharacteristics = paintingstr[1]
-                lstcharac = []
-                for x in range(2, nbofcharacteristics + 2):
-                    lstcharac.append(paintingstr[x])
-                p = Painting(type, id, lstcharac)
+                p = Painting(type, id)
+                print(p.id)
+                for x in range(2, int(nbofcharacteristics) + 2):
+
+                    if paintingstr[x] in characteristics:
+                        characteristics[paintingstr[x]].append(p)
+
+                        "Key exists"
+                    else:
+                        characteristics[paintingstr[x]] = [p]
 
 
 class Painting:
-    def __init__(self, type, id, characteristics):
+    def __init__(self, type, id):
         self.type = type
         self.id = id
-        self.characteristics = []
+
+
+class FrameGlass:
+    def __init__(self, paintings):
+        self.paintings = []
 
 
 # smile = [p1, p2, p3]
@@ -35,4 +47,5 @@ class Painting:
 # EXECUTION
 filenames = ['1_binary_landscapes.txt', '10_computable_moments.txt', '11_randomizing_paintings',
              '110_oily_portraits.txt', '0_example.txt']
-readfile(filenames)
+filenamesExample = ['0_example.txt']
+readfile(filenamesExample)
